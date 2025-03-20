99e7334ca91edf6605e5510f790c8f4a7957c6bb
Muhammad-Asif-Ansari <muhammadasifansari101@gmail.com>
2025-03-20 09:27:44 +0500

    update code

---
+++ src/components/header/Header.js
@@ -0,0 +1,28 @@
+import "./Header.css"
+function Header1(){
+  return (
+    <div>
+
+<nav class="nav">
+      <div class="container">
+        <h1 class="logo"><a href="/index.html">My Website</a></h1>
+        <ul>
+          <li><a href="#" class="current">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Services</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </div>
+    </nav>
+
+    <div class="hero">
+      <div class="container">
+        <h1>Welcome To My Website</h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores, consequuntur?</p>
+      </div>
+    </div>
+
+    </div>
+  )
+}
+export default Header1
